5fc7675cc88843eca68ae958163f8b4a59bcde96
KaelenB <ktbrown@ualberta.ca>
2023-01-14 12:49:28 -0700

    add todo endpoint

--- todo_list_backend/todo_api/views.py
+++ todo_list_backend/todo_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, views, response, status
+from .serializers import TodoSerializer
+from .models import TodoItem
+from .serializers import TodoSerializer
 
 # Create your views here.
+
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = TodoItem.objects.all()
--- todo_list_backend/todo_list_backend/urls.py
+++ todo_list_backend/todo_list_backend/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from todo_api import views
+
+router = routers.DefaultRouter()
+router.register(r'todo', views.TodoView, 'todo')
+router.register(r'list', views.TodoListView, 'list')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api', include(router.urls))
 ]

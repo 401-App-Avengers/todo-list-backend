6409a6d7c0d492a8ad39649fb84435b8200f3e15
KaelenB <ktbrown@ualberta.ca>
2023-01-14 11:57:25 -0700

    Added TodoItem

--- todo_list_backend/todo_api/models.py
+++ todo_list_backend/todo_api/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+# Create your models here.
+
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField()
+    created = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.title
